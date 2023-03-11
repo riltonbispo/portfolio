6ae82890e37e0788fbb4d762f8f40b5bbb527774
riltonbispo <riltonbispo1@gmail.com>
2023-03-11 13:22:10 -0300

    feat: logica nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { links } from "../../Data";
 import { FaGithub, FaLinkedinIn, FaDiscord } from "react-icons/fa";
-import {BsSun, BsMoon} from 'react-icons/bs'
-import './header.css'
+import { BsSun, BsMoon } from "react-icons/bs";
+import "./header.css";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('no-scroll', showMenu)
+
+  }, [showMenu])
+  
   return (
     <header className="header">
       <nav className="nav">
-        <a href="" className="nav__logo text-cs">Rilton</a>
-      
-        <div className="nav__menu">
+        <a href="" className="nav__logo text-cs">
+          Rilton
+        </a>
+
+        <div className={`${showMenu ? "nav__menu show-menu" : "nav__menu"}`}>
           <div className="nav__data">
             <ul className="nav__list">
-              {links.map(({name, path}, index) =>{
+              {links.map(({ name, path }, index) => {
                 return (
                   <li className="nav__item" key={index}>
-                    <a href='' className="nav__link text-cs">
+                    <a href="" className="nav__link text-cs">
                       {name}
                     </a>
                   </li>
-                )
+                );
               })}
             </ul>
             <div className="header__socials">
@@ -42,14 +51,19 @@ const Header = () => {
             <BsSun />
           </div>
 
-          <div className="nav__toggle">
+          <div
+            className={`${
+              showMenu ? "nav__toggle animate-toggle" : "nav__toggle"
+            }`}
+            onClick={() => setShowMenu(!showMenu)}
+          >
             <span></span>
             <span></span>
           </div>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
