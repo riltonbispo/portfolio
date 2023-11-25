6093293b70cfa3c62e91ad8ac2c397793e332cb2
rilton bispo <riltondev@gmail.com>
2023-11-25 12:59:23 -0300

    :sparkles: Add educational scheema

---
+++ schemas/educational.ts
@@ -0,0 +1,58 @@
+import { BiBookBookmark } from 'react-icons/bi'
+
+const educational = {
+  name: 'educational',
+  title: 'Educational',
+  type: 'document',
+  icon: BiBookBookmark,
+  fields: [
+    {
+      name: 'name',
+      title: 'School',
+      type: 'string',
+      description: 'Nome da instituição',
+    },
+    {
+      name: 'schoolTitle',
+      title: 'School Title',
+      type: 'string',
+      description: 'Especificação do Curso',
+    },
+    {
+      name: 'logo',
+      title: 'Instituição Logo',
+      type: 'image',
+    },
+    {
+      name: 'url',
+      title: 'Certificado',
+      type: 'url',
+    },
+    {
+      name: 'description',
+      title: 'Descrição do Curso',
+      type: 'text',
+      rows: 3,
+      description: 'Descreva suas atividades no curso',
+    },
+    {
+      name: 'skills',
+      title: 'Skills',
+      type: 'array',
+      description: 'Add a list of skills',
+      of: [{ type: 'string' }],
+    },
+    {
+      name: 'startDate',
+      title: 'Start Date',
+      type: 'date',
+    },
+    {
+      name: 'endDate',
+      title: 'End Date',
+      type: 'date',
+    },
+  ],
+}
+
+export default educational
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import profile from './profile'
 import job from './job'
 import project from './project'
+import educational from './educational'
 
-export const schemaTypes = [profile, job, project]
+export const schemaTypes = [profile, job, project, educational]
