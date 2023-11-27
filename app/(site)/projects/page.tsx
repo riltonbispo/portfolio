90612bf85f316a171d75173a3fb9369a827018d8
rilton bispo <riltondev@gmail.com>
2023-11-27 09:45:47 -0300

    reafctor: translate project page

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -10,14 +10,13 @@ export default async function Project() {
     <main className="max-w-7xl mx-auto md:px-16 px-6">
       <section className="max-w-2xl mb-16">
         <h1 className="text-3xl font-bold tracking-tight sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight">
-          Featured projects I&apos;ve built over the years
+          Projetos pessoais
         </h1>
         <p className="text-base text-zinc-400 leading-relaxed">
-          I&apos;ve worked on tons of little projects over the years but these
-          are the ones that I&apos;m most proud of. Many of them are
-          open-source, so if you see something that piques your interest, check
-          out the code and contribute if you have ideas for how it can be
-          improved.
+          Eu trabalhei em alguns projetos pessoais ao longo dos anos, mas estes
+          são os que mais me orgulho. Eles são de código aberto, então se algo
+          chamar sua atenção, confira o código e contribua se tiver ideias para
+          melhorias.
         </p>
       </section>
 
