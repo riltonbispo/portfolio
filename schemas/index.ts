30314b165e209d699785f07e184c60ac1eec91d0
riltonbispo <riltondev@gmail.com>
2024-01-14 11:44:17 -0300

    remove old code

--- schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import profile from './profile'
-import job from './job'
-import project from './project'
-import educational from './educational'
-
-export const schemaTypes = [profile, job, project, educational]
